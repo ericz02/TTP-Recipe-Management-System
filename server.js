71d103ddfa31142005422da0cde08008b8087e27
ericz02 <ezheng0720@gmail.com>
2023-07-12 21:54:42 -0400

    adds PATCH req

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/recipes", async (req, res) => {
 // get a specific recipe
 app.get("/recipes/:id", async (req, res) => {
   const recipeId = parseInt(req.params.id, 10);
-  
+
   try {
     const recipe = await RecipeApplication.findOne({ where: { id: recipeId } });
     
@@ -36,7 +36,25 @@ app.get("/recipes/:id", async (req, res) => {
       res.status(404).send({ message: "Recipe not found." })
     }
   } catch (err) {
-    console.error(err.message);
+    console.error(err);
     res.status(500).json({ error: err.message });
   }
 })
+
+// update a recipe
+app.patch("/recipes/:id", async (req, res) => {
+  const recipeId = parseInt(req.params.id, 10);
+
+  try {
+    const [numberOfAffectedRows, affectedRows] = await RecipeApplication.update(req.body, { where: { id: recipeId }, returning: true });
+
+    if (numberOfAffectedRows > 0) {
+      res.status(200).json(affectedRows[0]);
+    } else {
+      res.status(404).send({ message: "Recipe not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+    console.error(err);
+  }
+});
